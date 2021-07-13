b8476373f29525485a65dec4dee097c17a776496
disaster253 <65069829+disaster253@users.noreply.github.com>
2021-07-13 21:55:48 +0700

    Update main.js

--- bai4/main.js
+++ bai4/main.js
@@ -3,10 +3,8 @@ function divCreate() {
 		var div = $('<div id="' + i + '"></div>').appendTo("#divContent");
 		if (i % 2 == 0) {
 			$('<p class="bg-danger p-3 m-0 text-light">Div chẵn ' + i + '</p>').appendTo(div);
-
 		} else {
 			$('<p class="bg-primary p-3 m-0 text-light">Div lẻ ' + i + '</p>').appendTo(div);
-
 		}
 	}
 }
